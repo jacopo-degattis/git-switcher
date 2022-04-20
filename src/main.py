4f6ca972fcea5ca88748bd634034ca96d43ac3e8
Jacopo De Gattis <jacopo.degattis@gmail.com>
2022-04-20 11:20:30 +0200

    Add window to add new emails Write to file each time a new email is added

--- src/main.py
+++ src/main.py
@@ -9,10 +9,28 @@ class GitSwitchApp(object):
 
     def __init__(self):
         self.buttons = list()
+        self.win_text = None
+        self.win_clicked = None
         self.app = rumps.App("GitSwitch", icon="./git-logo.png")
-        self.config = self._load_config_file()
+        self.config = self._load_config_file()[0]
         self._create_entries_from_config()
         self._toggle_btns_from_value(subprocess.check_output(UPDATE_COMMAND).decode().strip())
+
+        self.window = rumps.Window(
+            message='Insert emails separated by a comma',
+            title='Add new email', default_text='test@example.com',
+            ok=None,
+            cancel=True,
+            dimensions=(200, 25)
+        )
+
+    def _write_to_file(self, email):
+        documents = None
+        with open("./config.yaml", "w") as config:
+            documents = yaml.dump([{"accounts": [*self.config["accounts"], str(email)]}], config)
+            # del self.app.menu["jacopo.degattis@gmail.com"]
+            self.app.menu.insert_after(self.config["accounts"][-1], rumps.MenuItem(title=str(email), callback=lambda x: print("CIAO")))
+        return documents
 
     def _load_config_file(self, config_file="./config.yaml"):
         data = None
@@ -33,11 +51,22 @@ class GitSwitchApp(object):
         for index, account in enumerate(accounts):
             self.buttons.append(rumps.MenuItem(title=account, callback=self._btn_callback, key=str(index+1)))
 
-        self.app.menu = self.buttons
+        self.app.menu = [*self.buttons, rumps.MenuItem(title="Test", callback=lambda _: self._handle_window())]
 
     def _toggle_btns_from_value(self, value):
         for entry in self.buttons:
             entry.state = 1 if entry.title == value else 0
+
+    def _handle_window(self):
+        response = self.window.run()
+        
+        if not response.clicked:
+            return
+        
+        if not response.text:
+            return
+
+        self._write_to_file(response.text)
 
     def _btn_callback(self, sender):
         subprocess.call([*UPDATE_COMMAND, sender.title])
